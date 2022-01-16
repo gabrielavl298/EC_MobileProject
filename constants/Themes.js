c79e92c0f9967b28cb61ca562ccf42fba6112f51
SynFunjioka <alanff2800@gmail.com>
2022-01-16 15:20:00 -0600

    nuevas carpetas

---
+++ constants/Themes.js
@@ -0,0 +1,42 @@
+export default{
+    COLORS: {
+        DEFAULT: '#DCDCDC',
+        PRIMARY: '#6E1B7D',
+        SECONDARY: '#BB69C9',
+        LABEL1: '#E38DA4',
+        LABEL2: '#FE2472',
+        INFO: '#00BCD4',
+        ERROR: '#F44336',
+        SUCCESS: '#4CAF50',
+        WARNING: '#FF9800',
+        MUTED: '#979797',
+        INPUT: {
+          BORDER: '#A3ADB5'
+        },
+        
+        ACTIVE: '#9C26B0',
+        BUTTON_COLOR: '#9C26B0',
+        PLACEHOLDER1: '#884C93',
+        PLACEHOLDER2: '#9FA5AA',
+        SWITCH_ON: '#9C26B0',
+        SWITCH_OFF: '#D4D9DD',
+        GRADIENT_START: '#6B24AA',
+        GRADIENT_END: '#AC2688',
+        PRICE_COLOR: '#EAD5FB',
+        BORDER_COLOR: '#E7E7E7',
+        BLOCK: '#E7E7E7',
+        ICON1: '#852196',
+        ICON2: '#4A4A4A',
+        OTHER:{
+          GOOGLEBTN: '#DD4B39'
+        }
+      },
+      SIZES: {
+        BLOCK_SHADOW_RADIUS: 2,
+        TEXT: {
+          TITLE: 25,
+          SUBTITLE:20,
+          NORMAL:15
+        }
+      }
+};
